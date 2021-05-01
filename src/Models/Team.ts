d2570835d61dc0d25b6d4b2eb8d93866344d23d8
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-05-01 13:22:31 -0300

    Adding Models and Table for Teams

--- src/Models/Team.ts
+++ src/Models/Team.ts
@@ -2,17 +2,22 @@ import {
     Column,
     CreateDateColumn,
     Entity,
+    OneToMany,
     PrimaryGeneratedColumn,
     UpdateDateColumn,
 } from 'typeorm';
+import { Category } from './Category';
 
-@Entity()
+@Entity({ name: 'teams' })
 export class Team {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column('varchar')
     name: string;
+
+    @OneToMany(() => Category, category => category.team)
+    categories: Array<Category>;
 
     @CreateDateColumn()
     created_at: Date;
