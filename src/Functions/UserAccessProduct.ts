43c298e5f5b1a9ac77b44011d9a5dec8c723895c
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-05-06 23:11:13 -0300

    Adding a function to check if user has access to a product

---
+++ src/Functions/UserAccessProduct.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+
+import UserTeam from '../App/Models/UserRoles';
+import ProductTeam from '../App/Models/ProductTeams';
+
+interface checkIfUserHasAccessToAProductProps {
+    product_id: string;
+    user_id: string;
+}
+
+export async function checkIfUserHasAccessToAProduct({
+    user_id,
+    product_id,
+}: checkIfUserHasAccessToAProductProps): Promise<boolean> {
+    const userTeamRepository = getRepository(UserTeam);
+    const productTeamRepository = getRepository(ProductTeam);
+
+    const userTeams = await userTeamRepository.find({
+        where: {
+            user: {
+                id: user_id,
+            },
+        },
+    });
+
+    const productTeam = await productTeamRepository.findOne({
+        where: {
+            product: {
+                id: product_id,
+            },
+        },
+        relations: ['team'],
+    });
+
+    const hasAccessToProduct = userTeams.filter(
+        userTeam => userTeam.team.id === productTeam?.team.id,
+    );
+
+    if (hasAccessToProduct.length > 0) {
+        return true;
+    }
+    return false;
+}
