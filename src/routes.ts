b21c78c1f0a0306aefbd3f2c09fa94746ab72f35
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-05-07 10:48:13 -0300

    Removing route to get all products by user

--- src/routes.ts
+++ src/routes.ts
@@ -4,7 +4,6 @@ import User from './App/Controllers/User';
 import Session from './App/Controllers/Session';
 import Product from './App/Controllers/Product';
 import Batch from './App/Controllers/Batch';
-import ProductByUser from './App/Controllers/ProductsByUser';
 import Team from './App/Controllers/Team';
 
 import AuthMiddleware from './App/Middlewares/Auth';
@@ -23,8 +22,6 @@ routes.use(AuthMiddleware);
 routes.get('/products/:id', Product.show);
 routes.post('/products', Product.create);
 routes.put('/products/:product_id', Product.update);
-
-routes.get('/products/:user/all', ProductByUser.getAll);
 
 routes.get('/batches/:id', Batch.index);
 routes.post('/batches', Batch.store);
