519496d8d92e2027adb012edb113fe955484230e
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2021-05-31 21:32:52 -0300

    Production prepare

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import App from './app';
 
 import './Services/Database';
 
-App.listen(3213, () => {
+const { PORT } = process.env;
+const { HOST } = process.env;
+
+App.listen(Number(PORT), HOST || 'localhost', () => {
     console.log('Server is running at 3213');
 });
