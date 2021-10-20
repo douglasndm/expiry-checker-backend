50ba462fb469671eb03510fa01e73f8dc5c1caca
Douglas Nunes de Mattos <douglasnunesdemattos@outlook.com>
2021-10-20 20:50:07 -0300

    fix: Brands import from classic app

--- src/@types/Utils/Brand.d.ts
+++ src/@types/Utils/Brand.d.ts
@@ -22,3 +22,17 @@ interface deleteBrandProps {
 interface getAllProductsFromBrand {
     brand_id: string;
 }
+
+interface createManyBrandsProps {
+    brands: ICVBrand[];
+    team_id: string;
+    user_id: string;
+}
+
+interface createManyBrandsResponse {
+    brands: {
+        id: string;
+        name: string;
+        old_id?: string;
+    }[];
+}
--- src/Utils/Brand.ts
+++ src/Utils/Brand.ts
@@ -144,3 +144,62 @@ export async function getAllProductsFromBrand({
 
     return products;
 }
+
+export async function createManyBrands({
+    brands,
+    team_id,
+    user_id,
+}: createManyBrandsProps): Promise<createManyBrandsResponse> {
+    const brandRepository = getRepository(Brand);
+
+    const userRole = await getUserRoleInTeam({
+        user_id,
+        team_id,
+    });
+
+    if (userRole !== 'manager' && userRole !== 'supervisor') {
+        throw new AppError({
+            message: "You don't have authorization",
+            internalErrorCode: 2,
+        });
+    }
+
+    const team = await getTeam({ team_id });
+    const allBrands = await getAllBrands({ team_id });
+
+    const brandsToCreate = brands.filter(brand => {
+        const exists = allBrands.find(
+            b => b.name.toLowerCase() === brand.name.toLowerCase(),
+        );
+
+        if (exists) {
+            return false;
+        }
+
+        return true;
+    });
+
+    const willCreate = brandsToCreate.map(brand => {
+        const newBrand = new Brand();
+        newBrand.name = brand.name;
+        newBrand.team = team;
+
+        return newBrand;
+    });
+
+    const createdBrands = await brandRepository.save(willCreate);
+
+    const response = createdBrands.map(brand => {
+        const old_id = brands.find(b => b.name === brand.name);
+
+        return {
+            old_id: old_id?.id,
+            name: brand.name,
+            id: brand.id,
+        };
+    });
+
+    return {
+        brands: response,
+    };
+}
