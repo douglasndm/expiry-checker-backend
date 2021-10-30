e28c85d1ad651b934d917b3067ba6568eb989ffe
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2021-10-29 23:06:26 -0300

    Adding a dev mode to bypass sentry

--- src/app.ts
+++ src/app.ts
@@ -12,18 +12,20 @@ import './Services/Cron';
 
 import App from './start';
 
-Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-        new Tracing.Integrations.Express({
-            app: App,
-        }),
-    ],
-    tracesSampleRate: 0.5,
-});
-App.use(Sentry.Handlers.requestHandler());
-App.use(Sentry.Handlers.tracingHandler());
+if (!process.env.DEV_MODE) {
+    Sentry.init({
+        dsn: process.env.SENTRY_DSN,
+        integrations: [
+            new Sentry.Integrations.Http({ tracing: true }),
+            new Tracing.Integrations.Express({
+                app: App,
+            }),
+        ],
+        tracesSampleRate: 0.5,
+    });
+    App.use(Sentry.Handlers.requestHandler());
+    App.use(Sentry.Handlers.tracingHandler());
+}
 
 App.use(rateLimiter);
 
