cc1e748ce5d1c6a070b3b92206209ae5fa579687
Douglas Nunes de Mattos <7841509+douglasndm@users.noreply.github.com>
2022-02-27 16:27:06 -0300

    Adding route to create store

--- src/App/Controllers/Stores/Store.ts
+++ src/App/Controllers/Stores/Store.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
 
 import { getAllStoresFromTeam } from '@utils/Stores/List';
+import { createStore } from '@utils/Stores/Create';
+import { getUserByFirebaseId } from '@utils/User';
 
+import AppError from '@errors/AppError';
+
+// Parent route (team) has a middleware to check if user is on team
+// so it is not necessary to check it here
 class StoreControle {
     async index(req: Request, res: Response): Promise<Response> {
         const { team_id } = req.params;
@@ -9,6 +15,28 @@ class StoreControle {
         const stores = await getAllStoresFromTeam({ team_id });
 
         return res.json(stores);
+    }
+
+    async create(req: Request, res: Response): Promise<Response> {
+        const { team_id } = req.params;
+        const { name } = req.body;
+
+        if (!req.userId) {
+            throw new AppError({
+                message: 'Provider your id',
+                internalErrorCode: 1,
+            });
+        }
+
+        const user = await getUserByFirebaseId(req.userId);
+
+        const createdStore = await createStore({
+            name,
+            team_id,
+            admin_id: user.id,
+        });
+
+        return res.status(201).json(createdStore);
     }
 }
 
--- src/Routes/stores.routes.ts
+++ src/Routes/stores.routes.ts
@@ -5,5 +5,6 @@ import Store from '@controllers/Stores/Store';
 const routes = Router({ mergeParams: true });
 
 routes.get('/', Store.index);
+routes.post('/', Store.create);
 
 export default routes;
