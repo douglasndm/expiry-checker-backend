6fc751a8b109916f4dc4db962b9e2d91393d8bde
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-03-27 22:50:27 -0300

    Adding route to delete subscription

--- src/App/Controllers/Subscription.ts
+++ src/App/Controllers/Subscription.ts
@@ -1,30 +1,23 @@
 import { Request, Response } from 'express';
-import * as Yup from 'yup';
 
 import { getSubscription } from '@utils/Subscriptions/Subscription';
-
-import AppError from '@errors/AppError';
+import { deleteSubscription } from '@utils/Subscriptions/Delete';
 
 class SubscriptionController {
     async index(req: Request, res: Response): Promise<Response> {
-        const schema = Yup.object().shape({
-            team_id: Yup.string().required().uuid(),
-        });
-
-        try {
-            await schema.validate(req.params);
-        } catch (err) {
-            if (err instanceof Error)
-                throw new AppError({
-                    message: err.message,
-                });
-        }
-
         const { team_id } = req.params;
 
         const subscription = await getSubscription(team_id);
 
         return res.json(subscription);
+    }
+
+    async delete(req: Request, res: Response): Promise<Response> {
+        const { team_id } = req.params;
+
+        await deleteSubscription(team_id);
+
+        return res.send();
     }
 }
 
