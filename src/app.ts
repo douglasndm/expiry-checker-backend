d660c06d2ff6af3932886639942544b669fd28b4
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-05-22 19:12:34 -0300

    Moving errors handle to app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
 import * as Sentry from '@sentry/node';
 import * as Tracing from '@sentry/tracing';
 import cors from 'cors';
+import 'express-async-errors';
 
 import rateLimiter from './App/Middlewares/RateLimiter';
 
@@ -10,6 +12,8 @@ import './Services/Firebase';
 import './Services/Redis';
 import './Services/Database';
 import './Services/Cron';
+
+import AppError from './Errors/AppError';
 
 import App from './start';
 
@@ -29,9 +33,26 @@ if (process.env.DEV_MODE === 'false') {
 }
 
 App.use(rateLimiter);
-
 App.use(cors());
 
 App.use(Routes);
+
+App.use(Sentry.Handlers.errorHandler());
+App.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response.status(err.statusCode).json({
+            status: 'error',
+            errorCode: err.errorCode,
+            message: err.message,
+        });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 export default App;
--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,7 @@
-import { Request, Response, NextFunction } from 'express';
-import * as Sentry from '@sentry/node';
-import 'express-async-errors';
-
-import AppError from '@errors/AppError';
 import App from './app';
 
 const { PORT } = process.env;
 const { HOST } = process.env;
-
-App.use(Sentry.Handlers.errorHandler());
-
-App.use((err: Error, request: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-            status: 'error',
-            errorCode: err.errorCode,
-            message: err.message,
-        });
-    }
-
-    console.error(err);
-
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error',
-    });
-});
 
 App.listen(Number(PORT), HOST || 'localhost', () => {
     console.log(`Server is running at ${HOST}:${PORT}`);
