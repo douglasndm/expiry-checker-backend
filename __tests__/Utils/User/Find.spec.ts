c21108bd1d9835ef70709badeb5ccb4125620263
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-05-22 23:03:19 -0300

    Adding errors expects for test of find user

--- __tests__/Utils/User/Find.spec.ts
+++ __tests__/Utils/User/Find.spec.ts
@@ -5,6 +5,8 @@ import {
     getUserByEmail,
 } from '@utils/User/Find';
 
+import AppError from '@errors/AppError';
+
 import connection from '../../Services/Database';
 
 beforeAll(async () => {
@@ -50,5 +52,39 @@ describe('Find user process', () => {
 
         expect(user).not.toBe(null);
         expect(user.name).toBe('Douglas');
+    });
+
+    // EXPECT ERRORS
+    it('should NOT find an user by firebase id', async () => {
+        try {
+            await getUserByFirebaseId('123');
+        } catch (err) {
+            expect(err).toBeInstanceOf(AppError);
+            if (err instanceof AppError) {
+                expect(err.errorCode).toBe(7);
+            }
+        }
+    });
+
+    it('should NOT find an user by id', async () => {
+        try {
+            await getUserById('08792baf-8264-4b1e-bd09-af922815e803');
+        } catch (err) {
+            expect(err).toBeInstanceOf(AppError);
+            if (err instanceof AppError) {
+                expect(err.errorCode).toBe(7);
+            }
+        }
+    });
+
+    it('should NOT find an user by email', async () => {
+        try {
+            await getUserByEmail('invalid@mail.com');
+        } catch (err) {
+            expect(err).toBeInstanceOf(AppError);
+            if (err instanceof AppError) {
+                expect(err.errorCode).toBe(7);
+            }
+        }
     });
 });
