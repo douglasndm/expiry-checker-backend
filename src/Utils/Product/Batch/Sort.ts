8310564dc4f2b7c363bc7d2ce3b13881620ff8d5
Douglas <7841509+douglasndm@users.noreply.github.com>
2022-06-24 23:09:58 -0300

    Boring conversion bug

--- src/Utils/Product/Batch/Sort.ts
+++ src/Utils/Product/Batch/Sort.ts
@@ -1,12 +1,12 @@
-import { compareAsc } from 'date-fns';
+import { compareAsc, parseISO } from 'date-fns';
 
 import Batch from '@models/Batch';
 
 export function sortBatchesByExpDate(batches: Array<Batch>): Array<Batch> {
     if (batches.length > 1) {
         const sortedBatches = batches.sort((batch1, batch2) => {
-            const date1 = batch1.exp_date;
-            const date2 = batch2.exp_date;
+            const date1 = parseISO(String(batch1.exp_date));
+            const date2 = parseISO(String(batch2.exp_date));
 
             if (compareAsc(date1, date2) > 0) return 1;
             if (compareAsc(date1, date2) < 0) return -1;
