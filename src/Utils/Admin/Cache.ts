d3e00f82f2d6bcd2d4ab391b6fac67b3a504c0ab
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-03-17 21:14:56 -0300

    Removing app version code

--- src/Utils/Admin/Cache.ts
+++ src/Utils/Admin/Cache.ts
@@ -1,14 +1,8 @@
 import Cache from '@services/Cache';
 
-async function clearVersionCache(): Promise<void> {
-    const cache = new Cache();
-
-    await cache.invalidade('latest_version');
-}
-
 async function clearAllCache(): Promise<void> {
     const cache = new Cache();
     await cache.invalidadeAllCache();
 }
 
-export { clearAllCache, clearVersionCache };
+export { clearAllCache };
