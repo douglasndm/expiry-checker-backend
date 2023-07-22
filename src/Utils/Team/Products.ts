fada1b7692b35c7eef498b1a4c723ea8e912e19d
Douglas <7841509+douglasndm@users.noreply.github.com>
2023-07-21 21:48:33 -0300

    Adding pagination of products

--- src/Utils/Team/Products.ts
+++ src/Utils/Team/Products.ts
@@ -12,28 +12,37 @@ import Product from '@models/Product';
 interface getProductsFromTeamProps {
     team_id: string;
     user_id: string;
+    page?: number;
+    removeCheckedBatches?: boolean;
+    sortByBatches?: boolean;
 }
 
 async function getProductsFromTeam({
     team_id,
     user_id,
+    page,
+    removeCheckedBatches,
+    sortByBatches,
 }: getProductsFromTeamProps): Promise<Product[]> {
     const cache = new Cache();
 
     const userStores = await getAllStoresFromUser({ user_id });
 
-    const cachedProds = await cache.get<Array<Product>>(
+    /* const cachedProds = await cache.get<Array<Product>>(
         `products-from-teams:${team_id}`,
-    );
+    ); */
+    // temp disable of cache
+    const cachedProds = null;
 
     let products: Product[] = [];
 
     if (!cachedProds) {
         const productTeamsRepository = getRepository(ProductTeams);
 
-        const productsTeam = await productTeamsRepository
+        let productsTeam: ProductTeams[] = [];
+
+        const query = productTeamsRepository
             .createQueryBuilder('product_teams')
-            .select('product_teams.id')
             .where('product_teams.team_id = :id', { id: team_id })
             .leftJoinAndSelect('product_teams.product', 'product')
             .leftJoinAndSelect('product.store', 'store')
@@ -41,12 +50,44 @@ async function getProductsFromTeam({
             .leftJoinAndSelect('product.categories', 'prodCat')
             .leftJoinAndSelect('prodCat.category', 'category')
             .leftJoinAndSelect('product.batches', 'batches')
-            .orderBy('batches.exp_date', 'ASC')
-            .getMany();
+            .select([
+                'product_teams.id',
+                'product.id',
+                'product.name',
+                'product.code',
+                'store.id',
+                'store.name',
+                'prodCat.id',
+                'category.id',
+                'category.name',
+                'brand.id',
+                'brand.name',
+
+                'batches.id',
+                'batches.name',
+                'batches.exp_date',
+                'batches.amount',
+                'batches.price',
+                'batches.status',
+                'batches.price_tmp',
+            ]);
+
+        if (sortByBatches && sortByBatches === true) {
+            query.orderBy('batches.exp_date', 'ASC');
+        }
+        if (removeCheckedBatches && removeCheckedBatches === true) {
+            query.andWhere('batches.status = :status', { status: 'unchecked' });
+        }
+
+        if (page !== undefined) {
+            query.take(20).skip(page * 20);
+        }
+
+        productsTeam = await query.getMany();
 
         products = productsTeam.map(p => p.product);
 
-        await cache.save(`products-from-teams:${team_id}`, products);
+        // await cache.save(`products-from-teams:${team_id}`, products);
     } else {
         products = cachedProds;
     }
