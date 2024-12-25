45ad465f2973613eede88b029eb2abffb79feda6
Douglas <7841509+douglasndm@users.noreply.github.com>
2024-12-25 08:36:06 -0300

    Improving user response

--- src/App/Controllers/User.ts
+++ src/App/Controllers/User.ts
@@ -6,10 +6,15 @@ import { defaultDataSource } from '@project/ormconfig';
 import { invalidadeCache } from '@services/Cache/Redis';
 
 import User from '@models/User';
+import UserTeam from '@models/UserTeam';
+import Store from '@models/Store';
 
-import { getUserByFirebaseId } from '@utils/User/Find';
+import { getUserByFirebaseId, getUserById } from '@utils/User/Find';
 import { createUser } from '@utils/User/Create';
 import { updateUser } from '@utils/User/Update';
+import { getTeamFromUser } from '@utils/User/Team';
+import { getUserRole } from '@utils/Team/Roles/Find';
+import { getSubscriptionFromTeam } from '@utils/Team/Subscription/Get';
 
 import { deleteUser } from '@functions/Users';
 
@@ -17,72 +22,88 @@ import AppError from '@errors/AppError';
 
 class UserController {
     async index(req: Request, res: Response): Promise<Response> {
-        if (!req.userId) {
+        if (!req.userUUID) {
             throw new AppError({
                 message: 'Provide the user id',
                 statusCode: 401,
                 internalErrorCode: 1,
             });
         }
+        const user = await getUserById(req.userUUID);
+        const userTeam = await getTeamFromUser(user.id);
+        let role: UserTeam | null = null;
 
-        const repository = defaultDataSource.getRepository(User);
-
-        const user = await repository
-            .createQueryBuilder('user')
-            .where('user.firebaseUid = :id', { id: req.userId })
-            .leftJoinAndSelect('user.role', 'role')
-            .leftJoinAndSelect('user.store', 'userStore')
-            .leftJoinAndSelect('userStore.store', 'store')
-            .leftJoinAndSelect('user.login', 'login')
-            .leftJoinAndSelect('role.team', 'team')
-            .leftJoinAndSelect('team.subscriptions', 'subscriptions')
-            .select([
-                'user',
-                'role.role',
-                'role.code',
-                'role.status',
-
-                'userStore',
-                'store.id',
-                'store.name',
-
-                'team.id',
-                'team.name',
-
-                'subscriptions.expireIn',
-                'subscriptions.membersLimit',
-                'subscriptions.isActive',
-            ])
-            .getOne();
-
-        if (!user) {
-            throw new AppError({
-                message: 'User not found',
-                statusCode: 401,
-                internalErrorCode: 7,
+        if (userTeam) {
+            role = await getUserRole({
+                user_id: user.id,
+                team_id: userTeam.team.id,
             });
         }
+        interface IResponseSubscription {
+            expireIn: string | Date;
+            membersLimit: number;
+        }
+        interface IResponse {
+            id: string;
+            fid: string;
+            email: string;
+            name?: string | null;
+            last_name?: string | null;
+            role?: {
+                role: string;
+                code: string | null;
+                status: string | null;
+                team: {
+                    id: string;
+                    name: string;
+                    subscriptions: IResponseSubscription[];
+                };
+            };
+            store?: Store | null;
+        }
 
-        let organizedUser = {
+        let response: IResponse = {
             id: user.id,
             fid: user.firebaseUid,
             email: user.email,
             name: user.name,
             last_name: user.lastName,
-            role: user.role,
         };
 
-        if (user.role) {
-            organizedUser = {
-                ...organizedUser,
-                store:
-                    user.store && user.role.role.toLowerCase() !== 'manager'
-                        ? user.store.store
-                        : null,
+        if (role && userTeam) {
+            const teamSubscription = await getSubscriptionFromTeam(
+                role.team.id,
+            );
+
+            const subscriptions: IResponseSubscription[] = [];
+
+            if (teamSubscription) {
+                subscriptions.push(teamSubscription);
+            }
+
+            let store: Store | null = null;
+
+            if (user.role && user.role.role.toLowerCase() !== 'manager') {
+                store = user.store ? user.store.store : null;
+            }
+
+            response = {
+                ...response,
+                role: {
+                    role: role.role,
+                    code: role.code,
+                    status: role.status,
+                    team: {
+                        id: role.team.id,
+                        name: userTeam.team.name || '',
+                        subscriptions,
+                    },
+                },
+                store,
             };
         }
 
-        return res.status(200).json(organizedUser);
+        return res.status(200).json(response);
     }
 
     async store(req: Request, res: Response): Promise<Response> {
