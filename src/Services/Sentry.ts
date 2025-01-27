949a73d3189ebeaa6205890162ce7f3892715783
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-01-27 01:38:29 -0300

    Adding profiling-node

--- src/Services/Sentry.ts
+++ src/Services/Sentry.ts
@@ -1,10 +1,12 @@
 import * as Sentry from '@sentry/node';
+import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 if (process.env.DEV_MODE === 'false') {
     // Ensure to call this before importing any other modules!
     Sentry.init({
         dsn: process.env.SENTRY_DSN,
-        // Performance Monitoring
+        integrations: [nodeProfilingIntegration()],
         tracesSampleRate: 1.0, //  Capture 100% of the transactions
+        profilesSampleRate: 1.0,
     });
 }
