e1379e7e6f878f4b47f7e694f89b6a10af311453
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-01-29 13:37:05 -0300

    fix: push notification send

--- src/Utils/Notifications/Push/Batch.ts
+++ src/Utils/Notifications/Push/Batch.ts
@@ -3,25 +3,27 @@ import Firebase from 'firebase-admin';
 import { captureException } from '@services/ExceptionsHandler';
 
 export interface ITokenMessagePush {
-    notification: {
-        title: string;
-        body: string;
-    };
-    data: {
-        deeplinking: string;
-    };
-    token: string;
+	notification: {
+		title: string;
+		body: string;
+	};
+	data: {
+		deeplinking: string;
+	};
+	token: string;
 }
 
 async function sendNotificationByFirebase(
-    messages: ITokenMessagePush[],
+	messages: ITokenMessagePush[]
 ): Promise<void> {
-    const messaging = Firebase.messaging();
-    const response = await messaging.sendAll(messages);
+	const messaging = Firebase.messaging();
+	const { responses, failureCount } = await messaging.sendEach(messages);
 
-    if (response.failureCount > 0) {
-        captureException(new Error('Failed to send notifications'), response);
-    }
+	if (failureCount > 0) {
+		captureException(new Error('Failed to send notifications'), {
+			responses,
+		});
+	}
 }
 
 export { sendNotificationByFirebase };
