3542507871d3d34481bbb4f41ed9a45eddead55c
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-01-29 18:25:27 -0300

    fix: null on code

--- src/App/Models/UserTeam.ts
+++ src/App/Models/UserTeam.ts
@@ -1,43 +1,43 @@
 import {
-    Column,
-    CreateDateColumn,
-    Entity,
-    JoinColumn,
-    ManyToOne,
-    OneToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
+	Column,
+	CreateDateColumn,
+	Entity,
+	JoinColumn,
+	ManyToOne,
+	OneToOne,
+	PrimaryGeneratedColumn,
+	UpdateDateColumn,
 } from 'typeorm';
 import Team from './Team';
 import User from './User';
 
 @Entity({ name: 'users_teams' })
 class UserTeam {
-    @PrimaryGeneratedColumn()
-    id: number;
+	@PrimaryGeneratedColumn()
+	id: number;
 
-    @OneToOne(() => User, user => user.role)
-    @JoinColumn({ name: 'user_id' })
-    user: User;
+	@OneToOne(() => User, user => user.role)
+	@JoinColumn({ name: 'user_id' })
+	user: User;
 
-    @ManyToOne(() => Team, team => team.users)
-    @JoinColumn({ name: 'team_id' })
-    team: Team;
+	@ManyToOne(() => Team, team => team.users)
+	@JoinColumn({ name: 'team_id' })
+	team: Team;
 
-    @Column()
-    role: string;
+	@Column()
+	role: string;
 
-    @Column({ name: 'enter_code' })
-    code: string;
+	@Column({ name: 'enter_code', type: 'varchar', nullable: true })
+	code: string | null;
 
-    @Column()
-    status: string;
+	@Column({ type: 'varchar', nullable: true })
+	status: string | null;
 
-    @CreateDateColumn()
-    created_at: Date;
+	@CreateDateColumn()
+	created_at: Date;
 
-    @UpdateDateColumn()
-    updated_at: Date;
+	@UpdateDateColumn()
+	updated_at: Date;
 }
 
 export default UserTeam;
