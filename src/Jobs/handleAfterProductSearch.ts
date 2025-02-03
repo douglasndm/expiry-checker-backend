b60543a07aae038044c7fb2df47d42ebdcc693b2
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-03 04:42:16 -0300

    Adding save of product on firestore too

--- src/Jobs/handleAfterProductSearch.ts
+++ src/Jobs/handleAfterProductSearch.ts
@@ -1,61 +1,69 @@
 import { defaultDataSource } from '@services/TypeORM';
 
+import { saveProductOnFirestore } from '@utils/ProductSearch/Save';
+
 import ProductDetails from '@models/ProductDetails';
 import ProductRequest from '@models/ProductRequest';
 
-interface handleAfterProductSearchProps {
-    data: { response: findProductByEANExternalResponse | null; code: string };
+interface Props {
+	data: { response: findProductByEANExternalResponse | null; code: string };
 }
 
-async function handleAfterProductSearch({
-    data,
-}: handleAfterProductSearchProps): Promise<void> {
-    const { response, code } = data;
+async function handleAfterProductSearch({ data }: Props): Promise<void> {
+	const { response, code } = data;
 
-    const productRepository = defaultDataSource.getRepository(ProductDetails);
-    const productRequestRepository =
-        defaultDataSource.getRepository(ProductRequest);
+	const productRepository = defaultDataSource.getRepository(ProductDetails);
+	const requestRepository = defaultDataSource.getRepository(ProductRequest);
 
-    const request = await productRequestRepository
-        .createQueryBuilder('request')
-        .where('request.code = :code', { code })
-        .getOne();
+	const request = await requestRepository
+		.createQueryBuilder('request')
+		.where('request.code = :code', { code })
+		.getOne();
 
-    if (request) {
-        if (response !== null) {
-            await productRequestRepository.remove(request);
-        } else {
-            request.rank += 1;
+	// Check if product is already on request table and add 1 to rank if
+	// we didn't find a response this time
+	if (request) {
+		if (response !== null) {
+			await requestRepository.remove(request);
+		} else {
+			request.rank += 1;
 
-            await productRequestRepository.save(request);
-        }
-    } else if (!response) {
-        if (code.trim().length >= 8) {
-            const productRequest = new ProductRequest();
-            productRequest.code = code.trim();
-            productRequest.rank = 1;
+			await requestRepository.save(request);
+		}
+	} else if (!response) {
+		if (code.trim().length >= 8) {
+			const productRequest = new ProductRequest();
+			productRequest.code = code.trim();
+			productRequest.rank = 1;
 
-            await productRequestRepository.save(productRequest);
-        }
-    } else if (response) {
-        const alreadyExists = await productRepository
-            .createQueryBuilder('product')
-            .where('product.code = :code', { code: response.code })
-            .getOne();
+			await requestRepository.save(productRequest);
+		}
+	} else if (response) {
+		await saveProductOnFirestore({
+			name: response.name,
+			code: response.code,
+			brand: response.brand || null,
+			image: response.thumbnail,
+		});
 
-        if (!alreadyExists) {
-            const newProduct = new ProductDetails();
-            newProduct.name = response.name;
-            newProduct.code = response.code;
-            newProduct.brand = response.brand;
-            newProduct.thumbnail = response.thumbnail;
+		const alreadyExists = await productRepository
+			.createQueryBuilder('product')
+			.where('product.code = :code', { code: response.code })
+			.getOne();
 
-            await productRepository.save(newProduct);
-        }
-    }
+		if (!alreadyExists) {
+			const newProduct = new ProductDetails();
+			newProduct.name = response.name;
+			newProduct.code = response.code;
+			newProduct.brand = response.brand;
+			newProduct.thumbnail = response.thumbnail;
+
+			await productRepository.save(newProduct);
+		}
+	}
 }
 
 export default {
-    key: 'HandleAfterProductSearch',
-    handle: handleAfterProductSearch,
+	key: 'HandleAfterProductSearch',
+	handle: handleAfterProductSearch,
 };
