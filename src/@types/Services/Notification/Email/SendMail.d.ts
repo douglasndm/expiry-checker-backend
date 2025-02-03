4e9b6f1fb24a96991270573d2d730145a34b5524
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-03 14:42:02 -0300

    Only sending up to 20 batches on mail notifications

--- src/@types/Services/Notification/Email/SendMail.d.ts
+++ src/@types/Services/Notification/Email/SendMail.d.ts
@@ -1,19 +1,19 @@
-interface batch {
-    team_id: string;
-    store?: Store;
-    code: string | null;
-    productName: string;
-    batch: string | null;
-    exp_date: string;
-    amount: number | null;
+interface MailBatch {
+	team_id: string;
+	store?: Store;
+	code: string | null;
+	productName: string;
+	batch: string | null;
+	exp_date: string;
+	amount: number | null;
 }
 
 interface MailNotification {
-    user_id: string;
-    to: string;
-    bcc?: string;
-    subject: string;
-    name: string;
-    AppName: string;
-    batches: batch[];
+	user_id: string;
+	to: string;
+	bcc?: string;
+	subject: string;
+	name: string;
+	AppName: string;
+	batches: MailBatch[];
 }
--- src/Services/Notification/Email/SendMail.ts
+++ src/Services/Notification/Email/SendMail.ts
@@ -1,154 +1,151 @@
 import { addDays, format, isBefore } from 'date-fns';
 
-import { getAllUsersIDAllowedToSendEmail } from '@services/Notification/Email';
 import BackgroundJob from '@services/Background';
+
+import { getTeamsWithActiveSubscriptions } from '@utils/Subscriptions/Actives';
+import { getUsersAllowedToSendMail } from '@utils/Notifications/Mail/UsersAllowed';
+
+import { getAllProductsFromManyTeams } from '@functions/Team/Products';
+import { getAllUsersIDAllowedToSendEmail } from './index';
 
 import UserTeam from '@models/UserTeam';
 import Store from '@models/Store';
 
-import { getAllUserRoles } from '@utils/UserRoles';
-import { getTeamsWithActiveSubscriptions } from '@utils/Subscriptions/Actives';
-
-import { getAllProductsFromManyTeams } from '@functions/Team/Products';
-
 async function sendMail(): Promise<void> {
-    const usersTeams = await getAllUserRoles();
+	const filtedUsersTeams = await getUsersAllowedToSendMail();
 
-    const allowedUsers = await getAllUsersIDAllowedToSendEmail();
+	// remove duplicades team before get all products
+	const teams: UserTeam[] = [];
 
-    const filtedUsersTeams = usersTeams.filter(item => {
-        const isAllowed = allowedUsers.find(
-            elemnt => elemnt.id === item.user.id,
-        );
+	filtedUsersTeams.forEach(item => {
+		const alreadyInArray = teams.find(i => i.team.id === item.team.id);
 
-        if (!isAllowed) {
-            return false;
-        }
-        return true;
-    });
+		if (!alreadyInArray) {
+			teams.push(item);
+		}
+	});
 
-    // remove duplicades team before get all products
-    const teams: UserTeam[] = [];
+	const teamsWithSubscriptions = await getTeamsWithActiveSubscriptions();
 
-    filtedUsersTeams.forEach(item => {
-        const alreadyInArray = teams.find(i => i.team.id === item.team.id);
+	const activeTeams = teams.filter(userRole => {
+		const finded = teamsWithSubscriptions.find(teamSub => {
+			if (teamSub.team.id === userRole.team.id) {
+				return true;
+			}
 
-        if (!alreadyInArray) {
-            teams.push(item);
-        }
-    });
+			return false;
+		});
 
-    const teamsWithSubscriptions = await getTeamsWithActiveSubscriptions();
+		return finded;
+	});
 
-    const activeTeams = teams.filter(userRole => {
-        const finded = teamsWithSubscriptions.find(teamSub => {
-            if (teamSub.team.id === userRole.team.id) {
-                return true;
-            }
+	const teamsIds = activeTeams.map(team => team.team.id);
 
-            return false;
-        });
+	// avoid cron crash if nobody wants email notification
+	if (teamsIds.length <= 0) {
+		return;
+	}
 
-        return finded;
-    });
+	const productsTeams = await getAllProductsFromManyTeams({
+		teams: teamsIds,
+	});
 
-    const teamsIds = activeTeams.map(team => team.team.id);
+	const batches: Array<MailBatch> = [];
 
-    // avoid cron crash if nobody wants email notification
-    if (teamsIds.length <= 0) {
-        return;
-    }
+	productsTeams.forEach(productTeam => {
+		if (productTeam.batches) {
+			const onlyExpOrNextBatches = productTeam.batches.filter(b => {
+				if (b.status === 'checked') return false;
 
-    const productsTeams = await getAllProductsFromManyTeams({
-        teams: teamsIds,
-    });
+				if (isBefore(b.exp_date, addDays(new Date(), 30))) {
+					return true;
+				}
+				return false;
+			});
 
-    const batches: Array<batch> = [];
+			onlyExpOrNextBatches.forEach(b => {
+				const alreadyInArray = batches.filter(
+					batch => batch.team_id !== productTeam.team.id
+				);
 
-    productsTeams.forEach(productTeam => {
-        if (productTeam.batches) {
-            const onlyExpOrNextBatches = productTeam.batches.filter(b => {
-                if (b.status === 'checked') return false;
+				if (alreadyInArray.length < 20) {
+					batches.push({
+						team_id: productTeam.team.id,
+						store: productTeam.store || undefined,
+						productName: productTeam.name,
+						code: productTeam.code || null,
+						amount: b.amount,
+						batch: b.name,
+						exp_date: format(b.exp_date, 'dd/MM/yyyy'),
+					});
+				}
+			});
+		}
+	});
 
-                if (isBefore(b.exp_date, addDays(new Date(), 30))) {
-                    return true;
-                }
-                return false;
-            });
+	const notifications: Omit<MailNotification, 'user_id'>[] = [];
 
-            onlyExpOrNextBatches.forEach(b => {
-                batches.push({
-                    team_id: productTeam.team.id,
-                    store: productTeam.store || undefined,
-                    productName: productTeam.name,
-                    code: productTeam.code || null,
-                    amount: b.amount,
-                    batch: b.name,
-                    exp_date: format(b.exp_date, 'dd/MM/yyyy'),
-                });
-            });
-        }
-    });
+	const allowedUsers = await getAllUsersIDAllowedToSendEmail();
 
-    const notifications: Omit<MailNotification, 'user_id'>[] = [];
+	allowedUsers.forEach(user => {
+		const userTeam = filtedUsersTeams.find(
+			item => item.user.id === user.id
+		);
 
-    allowedUsers.forEach(user => {
-        const userTeam = filtedUsersTeams.find(
-            item => item.user.id === user.id,
-        );
+		const userFilted = filtedUsersTeams.filter(
+			ut => ut.user.id === user.id
+		);
 
-        const userFilted = usersTeams.filter(ut => ut.user.id === user.id);
+		const userStores: Store[] = [];
 
-        const userStores: Store[] = [];
+		userFilted.forEach(u => {
+			if (u.user.store) {
+				userStores.push(u.user.store.store);
+			}
+		});
 
-        userFilted.forEach(u => {
-            if (u.user.store) {
-                userStores.push(u.user.store.store);
-            }
-        });
+		const teamBatches = batches.filter(b => {
+			if (userStores.length > 0) {
+				if (b.store) {
+					const userInStore = userStores.find(
+						store => store.id === b.store.id
+					);
 
-        const teamBatches = batches.filter(b => {
-            if (userStores.length > 0) {
-                if (b.store) {
-                    const userInStore = userStores.find(
-                        store => store.id === b.store.id,
-                    );
+					if (userInStore) {
+						return true;
+					}
 
-                    if (userInStore) {
-                        return true;
-                    }
+					return false;
+				}
+			} else if (b.team_id === userTeam?.team.id) {
+				return true;
+			}
 
-                    return false;
-                }
-            } else if (b.team_id === userTeam?.team.id) {
-                return true;
-            }
+			return false;
+		});
 
-            return false;
-        });
+		if (userTeam && userTeam.user.email) {
+			notifications.push({
+				name: userTeam.user.email || 'untitled',
+				to: userTeam.user.email,
+				subject: `Resumo semanal dos produtos (${userTeam?.team.name})`,
+				AppName: 'Controle de Validades',
+				batches: teamBatches,
+			});
+		}
+	});
 
-        if (userTeam && userTeam.user.email) {
-            notifications.push({
-                name: userTeam.user.email || 'untitled',
-                to: userTeam.user.email,
-                subject: `Resumo semanal dos produtos (${userTeam?.team.name})`,
-                AppName: 'Controle de Validades',
-                batches: teamBatches,
-            });
-        }
-    });
+	const notificationsWithBatches = notifications.filter(
+		notification => notification.batches.length > 0
+	);
 
-    const notificationsWithBatches = notifications.filter(
-        notification => notification.batches.length > 0,
-    );
-
-    if (process.env.DEV_MODE === 'false') {
-        notificationsWithBatches.forEach(async notification => {
-            await BackgroundJob.add('SendWeeklyMail', {
-                notification,
-            });
-        });
-    }
+	if (process.env.DEV_MODE === 'false') {
+		notificationsWithBatches.forEach(async notification => {
+			await BackgroundJob.add('SendWeeklyMail', {
+				notification,
+			});
+		});
+	}
 }
 
 export { sendMail };
