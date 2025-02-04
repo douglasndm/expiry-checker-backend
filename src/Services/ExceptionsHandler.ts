8a8237f14d407eeb3ec8f51384ca5fe370c4fc0b
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-04 02:26:58 -0300

    Improving exception handler

--- src/Services/ExceptionsHandler.ts
+++ src/Services/ExceptionsHandler.ts
@@ -1,13 +1,13 @@
 import * as Sentry from '@sentry/node';
 
 interface ICustomData {
-    [data: string]: unknown;
+	[data: string]: unknown;
 }
 
-function captureException(error: Error, customData?: ICustomData): void {
-    console.error(error);
+function captureException(error: unknown, customData?: ICustomData): void {
+	console.error(error);
 
-    Sentry.captureException(error, customData);
+	Sentry.captureException(error, customData);
 }
 
 export { captureException };
