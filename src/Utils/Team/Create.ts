bdfa689a05a1a473c9feda20ecc0e681e51b2a75
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-02-21 02:41:51 -0300

    Adding team creation, update and delete on firestore

--- src/Utils/Team/Create.ts
+++ src/Utils/Team/Create.ts
@@ -1,3 +1,5 @@
+import { firestore } from 'firebase-admin';
+
 import { defaultDataSource } from '@services/TypeORM';
 
 import Team from '@models/Team';
@@ -7,74 +9,84 @@ import User from '@models/User';
 import AppError from '@errors/AppError';
 
 async function createTeam({ name, admin_id }: createTeamProps): Promise<Team> {
-    const teamRepository = defaultDataSource.getRepository(Team);
-    const userRolesRepository = defaultDataSource.getRepository(UserTeam);
-    const userRepository = defaultDataSource.getRepository(User);
+	const teamRepository = defaultDataSource.getRepository(Team);
+	const userRolesRepository = defaultDataSource.getRepository(UserTeam);
+	const userRepository = defaultDataSource.getRepository(User);
 
-    const user = await userRepository.findOne({
-        where: {
-            firebaseUid: admin_id,
-        },
-    });
+	const user = await userRepository.findOne({
+		where: {
+			firebaseUid: admin_id,
+		},
+	});
 
-    if (!user) {
-        throw new AppError({
-            message: 'User was not found',
-            statusCode: 400,
-            internalErrorCode: 7,
-        });
-    }
+	if (!user) {
+		throw new AppError({
+			message: 'User was not found',
+			statusCode: 400,
+			internalErrorCode: 7,
+		});
+	}
 
-    // Check if user already has a team with the same name
-    const userTeams = await userRolesRepository
-        .createQueryBuilder('userTeams')
-        .leftJoinAndSelect('userTeams.team', 'team')
-        .leftJoinAndSelect('userTeams.user', 'user')
-        .where('user.firebaseUid = :user_id', { user_id: user?.firebaseUid })
-        .getMany();
+	// Check if user already has a team with the same name
+	const userTeams = await userRolesRepository
+		.createQueryBuilder('userTeams')
+		.leftJoinAndSelect('userTeams.team', 'team')
+		.leftJoinAndSelect('userTeams.user', 'user')
+		.where('user.firebaseUid = :user_id', { user_id: user?.firebaseUid })
+		.getMany();
 
-    const alreadyManager = userTeams.filter(
-        ur => ur.role.toLowerCase() === 'manager',
-    );
+	const alreadyManager = userTeams.filter(
+		ur => ur.role.toLowerCase() === 'manager'
+	);
 
-    if (alreadyManager.length > 0) {
-        throw new AppError({
-            message: 'You are already a manager of another team',
-            statusCode: 400,
-        });
-    }
+	if (alreadyManager.length > 0) {
+		throw new AppError({
+			message: 'You are already a manager of another team',
+			statusCode: 400,
+		});
+	}
 
-    const existsName = userTeams.filter(roles => {
-        if (roles.team.name) {
-            if (roles.team.name.toLowerCase() === name?.toLowerCase()) {
-                return true;
-            }
-        }
+	const existsName = userTeams.filter(roles => {
+		if (roles.team.name) {
+			if (roles.team.name.toLowerCase() === name?.toLowerCase()) {
+				return true;
+			}
+		}
 
-        return false;
-    });
+		return false;
+	});
 
-    if (existsName.length > 0) {
-        throw new AppError({
-            message: 'You already have a team with the same name',
-            statusCode: 400,
-            internalErrorCode: 14,
-        });
-    }
+	if (existsName.length > 0) {
+		throw new AppError({
+			message: 'You already have a team with the same name',
+			statusCode: 400,
+			internalErrorCode: 14,
+		});
+	}
 
-    const team = new Team();
-    team.name = name;
+	const team = new Team();
+	team.name = name;
 
-    const savedTeam = await teamRepository.save(team);
+	const savedTeam = await teamRepository.save(team);
 
-    const userRole = new UserTeam();
-    userRole.team = savedTeam;
-    userRole.user = user;
-    userRole.role = 'Manager';
+	const userRole = new UserTeam();
+	userRole.team = savedTeam;
+	userRole.user = user;
+	userRole.role = 'manager';
 
-    await userRolesRepository.save(userRole);
+	await userRolesRepository.save(userRole);
 
-    return savedTeam;
+	// save on firestore
+	const teamsCollection = firestore().collection('teams');
+
+	await teamsCollection.doc(savedTeam.id).set({
+		name,
+
+		createdAt: new Date(),
+		updatedAt: new Date(),
+	});
+
+	return savedTeam;
 }
 
 export { createTeam };
---
+++ src/Utils/Team/Update.ts
@@ -0,0 +1,54 @@
+import { firestore } from 'firebase-admin';
+
+import { defaultDataSource } from '@services/TypeORM';
+import { invalidadeTeamCache } from '@services/Cache/Redis';
+
+import Team from '@models/Team';
+
+import AppError from '@errors/AppError';
+
+interface IUpdateTeam {
+	team_id: string;
+	team_name: string;
+}
+
+async function updateTeam(team: IUpdateTeam): Promise<Team> {
+	const { team_id, team_name } = team;
+
+	const teamRepository = defaultDataSource.getRepository(Team);
+
+	const teamUpdated = await teamRepository
+		.createQueryBuilder('team')
+		.where('team.id = :team_id', { team_id: team_id })
+		.getOne();
+
+	if (!teamUpdated) {
+		throw new AppError({
+			message: 'Team not found',
+			internalErrorCode: 6,
+		});
+	}
+
+	teamUpdated.name = team_name;
+
+	await teamRepository.save(teamUpdated);
+
+	const firestoreTeam = await firestore()
+		.collection('teams')
+		.doc(team_id)
+		.get();
+
+	if (firestoreTeam.exists) {
+		await firestoreTeam.ref.update({
+			name: team_name,
+
+			updatedAt: new Date(),
+		});
+	}
+
+	await invalidadeTeamCache(team_id);
+
+	return teamUpdated;
+}
+
+export { updateTeam };
