4fce0a0d9ac4d6b7d38b40be948046940f78b980
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-03-19 23:51:02 -0300

    fix: return null on user does not have subscription

--- src/App/Controllers/Subscription.ts
+++ src/App/Controllers/Subscription.ts
@@ -1,35 +1,49 @@
 import { Request, Response } from 'express';
 
 import {
-    getExternalSubscriptionByTeamId,
-    getSubscription,
+	getExternalSubscriptionByTeamId,
+	getSubscription,
 } from '@utils/Subscriptions/Subscription';
 import { deleteSubscription } from '@utils/Subscriptions/Delete';
 
+import AppError from '@errors/AppError';
+
 class SubscriptionController {
-    async index(req: Request, res: Response): Promise<Response> {
-        const { team_id } = req.params;
+	async index(req: Request, res: Response): Promise<Response> {
+		const { team_id } = req.params;
 
-        const subscription = await getSubscription(team_id);
+		try {
+			const subscription = await getSubscription(team_id);
 
-        return res.json(subscription);
-    }
+			return res.json(subscription);
+		} catch (error) {
+			if (error instanceof AppError) {
+				if (error.errorCode === 5) {
+					return res.json(null);
+				}
 
-    async delete(req: Request, res: Response): Promise<Response> {
-        const { team_id } = req.params;
+				throw error;
+			}
+		}
 
-        await deleteSubscription(team_id);
+		return res.json(null);
+	}
 
-        return res.send();
-    }
+	async delete(req: Request, res: Response): Promise<Response> {
+		const { team_id } = req.params;
 
-    async storeData(req: Request, res: Response): Promise<Response> {
-        const { team_id } = req.params;
+		await deleteSubscription(team_id);
 
-        const response = await getExternalSubscriptionByTeamId(team_id);
+		return res.send();
+	}
 
-        return res.json(response);
-    }
+	async storeData(req: Request, res: Response): Promise<Response> {
+		const { team_id } = req.params;
+
+		const response = await getExternalSubscriptionByTeamId(team_id);
+
+		return res.json(response);
+	}
 }
 
 export default new SubscriptionController();
