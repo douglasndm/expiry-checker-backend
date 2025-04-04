32de72fd65a9c50d6feae79ed37855ed4abdcfe9
Douglas <7841509+douglasndm@users.noreply.github.com>
2025-04-04 10:20:47 -0300

    adding admin route to force confirm user mail

--- src/Admin/routes/user.routes.ts
+++ src/Admin/routes/user.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import find from '@admin/controllers/user/find';
+import mailConfirm from '@admin/controllers/user/mailConfirm';
 
 const routes = Router({ mergeParams: true });
 
 routes.get('/find', find.index);
+routes.put('/mail/:email/confirm', mailConfirm.update);
 
 export default routes;
